ab56a03098021a1a607a8757336d06b7d06d3d6b
zansky07 <mhdzans02@gmail.com>
2021-10-06 18:40:40 +0700

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts/admin')
+
+@section('content')
+<div class="container">
+    <h3>GRID 1</h3>
+  <div class="row  justify-content-start">
+    <div class=" bg-primary text-white p-2 rounded">
+      Kolom 1
+    </div>
+    <div class=" bg-secondary text-white p-2 rounded">
+      Kolom 2
+    </div>
+    <div class=" bg-info text-white p-2 rounded">
+      Kolom 3
+    </div>
+    <div class=" bg-danger text-white p-2 rounded">
+      Kolom 4
+    </div>
+  </div>
+  <h3>GRID 2</h3>
+  <div class="row justify-content-center">
+     
+  <div class=" bg-primary text-white p-2 rounded">
+      Kolom 1
+    </div>
+    <div class=" bg-secondary text-white p-2 rounded">
+      Kolom 2
+    </div>
+    <div class=" bg-info text-white p-2 rounded">
+      Kolom 3
+    </div>
+    <div class=" bg-danger text-white p-2 rounded">
+      Kolom 4
+    </div>
+  </div>
+  <h3>GRID 3</h3>
+  <div class="row justify-content-end">
+      
+  <div class=" bg-primary text-white p-2 rounded">
+      Kolom 1
+    </div>
+    <div class=" bg-secondary text-white p-2 rounded">
+      Kolom 2
+    </div>
+    <div class=" bg-info text-white p-2 rounded">
+      Kolom 3
+    </div>
+    <div class=" bg-danger text-white p-2 rounded">
+      Kolom 4
+    </div>
+  </div>
+  <h3>GRID 4</h3>
+  <div class="row justify-content-around">
+     
+  <div class=" bg-primary text-white p-2 rounded">
+      Kolom 1
+    </div>
+    <div class=" bg-secondary text-white p-2 rounded">
+      Kolom 2
+    </div>
+    <div class=" bg-info text-white p-2 rounded">
+      Kolom 3
+    </div>
+    <div class=" bg-danger text-white p-2 rounded">
+      Kolom 4
+    </div>
+  </div>
+  <h3>GRID 5</h3>
+  <div class="row justify-content-between">
+     
+  <div class=" bg-primary text-white p-2 rounded">
+      Kolom 1
+    </div>
+    <div class=" bg-secondary text-white p-2 rounded">
+      Kolom 2
+    </div>
+    <div class=" bg-info text-white p-2 rounded">
+      Kolom 3
+    </div>
+    <div class=" bg-danger text-white p-2 rounded">
+      Kolom 4
+    </div>
+  </div>
+  <h3>GRID 7</h3>
+  <div class="row justify-content-between">
+     
+  <div class="col-lg-1 bg-primary text-white p-2 rounded">
+      Kolom 1
+    </div>
+    <div class="col-lg-2 bg-secondary text-white p-2 rounded">
+      Kolom 2
+    </div>
+    <div class="col-lg-3 bg-info text-white p-2 rounded">
+      Kolom 3
+    </div>
+    <div class="col-lg-4 bg-danger text-white p-2 rounded">
+      Kolom 4
+    </div>
+  </div>
+</div>
+@endsection
